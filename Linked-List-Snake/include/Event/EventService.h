1e130cbaf159517c09755f33e4c968ed53a7369f
nayan <nayankamalverma@gmail.com>
2024-07-13 22:53:35 +0530

    updated event service

--- Linked-List-Snake/include/Event/EventService.h
+++ Linked-List-Snake/include/Event/EventService.h
@@ -19,12 +19,22 @@ namespace Event
 
 		ButtonState left_mouse_button_state;
 		ButtonState right_mouse_button_state;
+		ButtonState left_arrow_button_state;
+		ButtonState right_arrow_button_state;
+		ButtonState up_arrow_button_state;
+		ButtonState down_arrow_button_state;
+		ButtonState W_button_state;
+		ButtonState S_button_state;
+		ButtonState A_button_state;
+		ButtonState D_button_state;
 
 		bool isGameWindowOpen();
 		bool gameWindowWasClosed();
 		bool hasQuitGame();
 		bool isKeyboardEvent();
 		void updateMouseButtonsState(ButtonState& current_button_state, sf::Mouse::Button mouse_button);
+
+		void updateKeyboardButtonsState(ButtonState& current_button_state, sf::Keyboard::Key keyboard_button);
 
 	public:
 		EventService();
@@ -39,6 +49,10 @@ namespace Event
 		bool pressedRightArrowKey();
 		bool pressedUpArrowKey();
 		bool pressedDownArrowKey();
+		bool pressedWKey();
+		bool pressedAKey();
+		bool pressedSKey();
+		bool pressedDKey();
 		bool pressedLeftMouseButton();
 		bool pressedRightMouseButton();
 	};
