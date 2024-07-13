079b4fba195e0cb514451e84474798684cf501d7
nayan <nayankamalverma@gmail.com>
2024-07-13 17:01:06 +0530

    updated bodyPart class

---
+++ Linked-List-Snake/source/Player/BodyPart.cpp
@@ -0,0 +1,54 @@
+﻿#include "Player/BodyPart.h"
+#include "Global/ServiceLocator.h"
+#include "Level/LevelView.h"
+#include "Level/LevelModel.h"
+#include "Global/Config.h"
+
+namespace Player
+{
+	using namespace Global;
+	using namespace Level;
+	using namespace UI::UIElement;
+
+	BodyPart::BodyPart()
+	{
+		grid_position = sf::Vector2i(0, 0);
+		createBodyPartImage();
+	}
+
+	BodyPart::~BodyPart()
+	{
+		destroy();
+	}
+
+	void BodyPart::initialize(float width, float height, sf::Vector2i pos, Direction dir)
+	{
+		bodypart_width = width;
+		bodypart_height = height;
+		direction = dir;
+		grid_position = pos;
+
+		initializeBodyPartImage();
+	}
+
+	void BodyPart::createBodyPartImage()
+	{
+		bodypart_image = new ImageView();
+	}
+
+	void BodyPart::initializeBodyPartImage()
+	{
+		bodypart_image->initialize(Config::snake_body_texture_path, bodypart_width, bodypart_height, (sf::Vector2f)grid_position);
+		bodypart_image->setOriginAtCentre();
+	}
+
+	void BodyPart::render()
+	{
+		bodypart_image->render();
+	}
+
+	void BodyPart::destroy()
+	{
+		delete (bodypart_image);
+	}
+}
