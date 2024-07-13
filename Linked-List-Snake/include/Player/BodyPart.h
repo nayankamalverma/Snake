dc4d04d5f4e4cfee473fab6b2060b77433d1b2c3
nayan <nayankamalverma@gmail.com>
2024-07-13 16:43:41 +0530

    created bodypart class

---
+++ Linked-List-Snake/include/Player/BodyPart.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+#include "UI/UIElement/ImageView.h"
+#include "Direction.h"
+
+namespace Player
+{
+
+	class BodyPart
+	{
+	protected:
+		UI::UIElement::ImageView* bodypart_image;
+
+		sf::Vector2i grid_position;
+		Player::Direction direction;
+
+		float bodypart_width;
+		float bodypart_height;
+	};
+}
