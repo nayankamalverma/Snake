5adb162f5d1bb503d63efdfcdd9f322d4aecd0db
nayan <nayankamalverma@gmail.com>
2024-07-13 19:41:26 +0530

    implemented snake movement logic

--- Linked-List-Snake/source/Player/SnakeController.cpp
+++ Linked-List-Snake/source/Player/SnakeController.cpp
@@ -6,6 +6,7 @@ namespace Player
 {
 	using namespace LinkedList;
 	using namespace Global;
+	using namespace Event;
 
 	SnakeController::SnakeController()
 	{
@@ -52,11 +53,34 @@ namespace Player
 		single_linked_list->render();
 	}
 
-	void SnakeController::processPlayerInput() { }
+	void SnakeController::processPlayerInput()
+	{
+		EventService* event_service = ServiceLocator::getInstance()->getEventService();
 
-	void SnakeController::updateSnakeDirection() { }
+		if (event_service->pressedUpArrowKey() && current_snake_direction != Direction::DOWN)
+		{
+			current_snake_direction = Direction::UP;
+		}
+		else if (event_service->pressedDownArrowKey() && current_snake_direction != Direction::UP)
+		{
+			current_snake_direction = Direction::DOWN;
+		}
+		else if (event_service->pressedLeftArrowKey() && current_snake_direction != Direction::RIGHT)
+		{
+			current_snake_direction = Direction::LEFT;
+		}
+		else if (event_service->pressedRightArrowKey() && current_snake_direction != Direction::LEFT)
+		{
+			current_snake_direction = Direction::RIGHT;
+		}
+	}
 
-	void SnakeController::moveSnake() { }
+	void SnakeController::updateSnakeDirection()
+	{
+		single_linked_list->updateNodeDirection(current_snake_direction);
+	}
+
+	void SnakeController::moveSnake() { single_linked_list->updateNodePosition(); }
 
 	void SnakeController::processSnakeCollision() { }
 
